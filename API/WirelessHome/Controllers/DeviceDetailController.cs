3757e7e1b3766a64834967eebac33d9163b28809
krzyzowskamartyna <krzyzowskamartyna@gmail.com>
2019-05-16 07:27:11 +0200

    fix

--- API/WirelessHome/Controllers/DeviceDetailController.cs
+++ API/WirelessHome/Controllers/DeviceDetailController.cs
@@ -131,33 +131,8 @@ namespace WirelessHome.Controllers
         {
             return _context.DeviceDetail.Any(e => e.Id == id);
         }
+  }
+        
 
-        public static void ComplexPing(string ip)
-        {
-            Ping pingSender = new Ping();
-
-            string data = "testping";
-            byte[] buffer = Encoding.ASCII.GetBytes(data);
-
-            int timeout = 10000;
-
-            PingOptions options = new PingOptions(64, true);
-
-            PingReply reply = pingSender.Send(ip, timeout, buffer, options);
-
-            if (reply.Status == IPStatus.Success)
-            {
-                Console.WriteLine("Address: {0}", reply.Address.ToString());
-                Console.WriteLine("RoundTrip time: {0}", reply.RoundtripTime);
-                Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
-                Console.WriteLine("Don't fragment: {0}", reply.Options.DontFragment);
-                Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
-            }
-            else
-            {
-                Console.WriteLine(reply.Status);
-            }
-        }
-
-    }
+    
 }
