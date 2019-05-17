f4dc069bb9a3b5ecfc05a81c7c776e6fc1431b49
krzyzowskamartyna <krzyzowskamartyna@gmail.com>
2019-05-17 12:06:42 +0200

    config fix

--- Angular/src/app/control-page/control-page.component.ts
+++ Angular/src/app/control-page/control-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { apiService } from '../services/api.service';
 import { Detail } from '../services/detail.model';
 import { Paho } from 'ng2-mqtt/mqttws31';
+import { config } from '../config.js';
 
 @Component({
   selector: 'app-control-page',
@@ -13,32 +14,20 @@ export class ControlPageComponent implements OnInit {
   private MQTT_HOST = '192.168.0.50';
   private MQTT_PORT = 9001;
   private MQTT_TOPIC = 'esp/dev0';
-  private MQTT_MSG = 'on';
 
-  options = {
-    host: this.MQTT_HOST,
-    port: this.MQTT_PORT,
-    username: 'mqtt_user',
-    password: 'password'
-
-  }
   client: any;
   constructor(private api: apiService) {
-
   }
   message: any;
   ngOnInit() {
     this.loadList();
     this.client = new Paho.MQTT.Client(this.MQTT_HOST, Number(this.MQTT_PORT), 'clientId');
     this.client.onConnectionLost = this.onConnectionLost.bind(this);
-    this.client.connect({ onSuccess: this.onConnect.bind(this), userName: 'mqtt_user', password: 'password' });
-
+    this.client.connect({ onSuccess: this.onConnect.bind(this), userName: config.userName, password: config.password });
   }
 
   onConnect() {
-    console.log("onConnect");
     this.client.subscribe(this.MQTT_TOPIC);
-    console.log('subscribed');
   }
   onConnectionLost(responseObject) {
     if (responseObject.errorCode !== 0) {
@@ -49,9 +38,7 @@ export class ControlPageComponent implements OnInit {
   turn(msg: string, element: Detail) {
     this.message = new Paho.MQTT.Message(msg);
     this.message.destinationName = element.MqttTopics;
-    console.log(element.MqttTopics);
     this.client.send(this.message);
-    console.log('send')
   }
 
   list: Detail[] = [];
